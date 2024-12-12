54eb912fe6f5540895f8d64f16275dc44723ba16
drumilbhati <drumilbhati5@gmail.com>
2024-12-12 06:57:04 +0530

    feat: added dummy Home.jsx for reference

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { AuthProvider } from './utils/AuthContext.jsx';
 import App from './App.jsx';
 import { BrowserRouter} from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <AuthProvider>
 	<React.StrictMode>
     <BrowserRouter>
 	    <App/>
     </BrowserRouter>
 	</React.StrictMode>
-  </AuthProvider>,
 );
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import '../App.css';
+const Home = () => {
+    return (
+        <div>
+        <h1 className='title'>Home</h1>
+        </div>
+    );
+};
+
+export default Home;
