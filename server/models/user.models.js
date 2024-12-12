00f2c6a7105805ff5c68ede149c76cdc0258638f
drumilbhati <drumilbhati5@gmail.com>
2024-12-12 06:43:15 +0530

    feat: migrate project to Vite with React and remove TypeScript configuration

---
+++ server/models/user.models.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: 'user'
+    },
+    avatar: {
+        type: String,
+        default: 'https://res.cloudinary.com/dxkufsejm/image/upload/v1627481523/avatar/avatar_cugq40.png'
+    },
+    age: {
+        type: Number,
+        default: 0
+    },
+    major: {
+        type: String,
+        default: ''
+    },
+    school: {
+        type: String,
+        default: ''
+    },
+    description: {
+        type: String,
+        default: ''
+    },
+    followers: [{ type: mongoose.Types.ObjectId, ref: 'user' }],
+    following: [{ type: mongoose.Types.ObjectId, ref: 'user' }],
+    skills: {
+        type: Array,
+        default: []
+    },
+    projects: [{ type: mongoose.Types.ObjectId, ref: 'project' }],
+}, { timestamps: true });
