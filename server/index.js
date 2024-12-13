5e5cb988ea3e1487251c534c6e2e2449008f5e73
drumilbhati <drumilbhati5@gmail.com>
2024-12-13 17:12:30 +0530

    feat: add Socket.IO for real-time chat functionality and create basic chat interface notice: ignore index.html in server

--- server/index.js
+++ server/index.js
@@ -3,10 +3,17 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import userRouter from './routers/user.routers.js';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
 
 dotenv.config();
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
 const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
@@ -21,6 +28,19 @@ mongoose.connect(process.env.MONGO_URI)
     process.exit(1);
 });
 
-app.listen(port, () => {
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected', socket.id);
+    socket.on('chat message', (message) => {
+        io.emit('chat message', message);
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
