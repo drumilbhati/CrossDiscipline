e7f33e49cfde9ccb6ba80b70f3b97b707d020446
drumilbhati <drumilbhati5@gmail.com>
2024-12-16 11:10:43 +0530

    feat: add chat button to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,11 @@ const Home = () => {
     return (
         <div>
         <h1 className='title'>Home</h1>
+        <button 
+            onClick={() => window.location.href = '/chat'}>
+                Chat
+            </button>
+        
         </div>
     );
 };
