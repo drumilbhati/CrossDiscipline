90a15d084a13873f268f1b040a8c72039508327d
Devarsh Doshi <devarsh.d2@ahduni.edu.in>
2024-12-19 12:03:43 +0530

    Edited

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/Home.jsx';
 import Chat from './pages/Chat';
 import './App.css';
+import LoginPage from './pages/LoginPage';
+import Signup from './pages/Signup';
+
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chat" element={<Chat />} />
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<LoginPage/>}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
       </Routes>
     </>
   );
-};
+};  
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,19 @@
+import { Navigate, useNavigate } from 'react-router-dom';
 import '../App.css';
+
 const Home = () => {
+    const navigate=useNavigate();
+    const handleClick=()=>{
+        
+        navigate("/login");
+        console.log("hi");
+    }
     return (
+       
         <div>
         <h1 className='title'>Home</h1>
+        
+        <button className='login' onClick={handleClick}>Login</button>
         </div>
     );
 };
