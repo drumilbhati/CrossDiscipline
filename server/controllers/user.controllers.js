11f67cbf1044dbaca0d948a88cbbd4e7d516c13e
Drumil <drumilbhati5@gmail.com>
2025-05-26 09:43:01 +0530

    added findUser

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -8,9 +8,9 @@ export const createUser = async (req, res) => {
     try {
         const { username, email, password, role } = req.body;
 
-        let user = await User.findOne({ username });
+        let user = await User.findOne({ email });
         if (user) {
-            return res.status(400).json({ error: 'Username already exists' });
+            return res.status(400).json({ error: 'Email already exists' });
         }
 
         const salt = await bcrypt.genSalt(10);
@@ -31,3 +31,26 @@ export const createUser = async (req, res) => {
         res.status(500).json({ error: 'Server error' });
     }
 };
+
+export const findUser = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        let user = User.findOne({ email });
+
+        if (user) {
+            const salt = bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            
+            if (hashedPassword == user.password) {
+                res.status(200).json(user);
+            } else {
+                res.status(403).json({ message: 'Incorrect password' });
+            }
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        console.error("Error finding error", error);
+        res.status(500).json({ error: 'Server error '});
+    }
+}
--- server/routers/user.routers.js
+++ server/routers/user.routers.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createUser } from '../controllers/user.controllers.js';
+import { createUser, findUser } from '../controllers/user.controllers.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/api/create-user', createUser);
+userRouter.get('/api/find-user', findUser);
 
 export default userRouter;
